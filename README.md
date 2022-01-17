a2f2ba7df0031a86aa47d650b7ab4ccd39c398d8
David Baum <baum@cqse.eu>
2022-01-17 19:11:03 +0100

    TS-25312 draft version

---
+++ README.md
@@ -0,0 +1,21 @@
+# Teamscale Upload
+
+An action for uploading external analysis results (coverage, findings, …) to Teamscale. Based on [teamscale-upload](https://github.com/cqse/teamscale-upload).
+
+# Usage 
+
+See [action.yml](action.yml)
+
+```yaml
+- uses: 'cqse/teamscale-upload-action@main'
+    with:
+      server: 'https://demo.teamscale.com'
+      project: 'teamscale-upload'
+      user: 'build'
+      partition: 'Github Action > Linux Branch And Timestamp'
+      accesskey: ${{ secrets.ACCESS_KEY }}
+      format: 'SIMPLE'
+      branch-and-timestamp: 'master:HEAD'
+      message: 'This is a test message.'
+      files: 'test_resources/coverage.simple test_resources/coverage2.simple'
+```
---
+++ src/run-teamscale-upload.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [[ "$OS" == "Windows" ]]; then
+  unzip teamscale-upload-windows.zip;
+else
+  unzip teamscale-upload-linux.zip
+fi
+chmod +x teamscale-upload;
+
+# mandatory arguments that have to be always present
+ARGS=( "--server" "$SERVER" "--project" "$PROJECT" "--user" "$USER" "--partition" "$PARTITION" "--accesskey" "$ACCESSKEY" "--format" "$FORMAT" )
+
+# optional parameters. We only use them if they have been set
+if [ -n "$REVISION" ]; then
+  ARGS+=( "--commit" "$REVISION" )
+fi
+if [ -n "$BRANCH_AND_TIMESTAMP" ]; then
+  ARGS+=( "--branch-and-timestamp" "$BRANCH_AND_TIMESTAMP" )
+fi
+if [ -n "$MESSAGE" ]; then
+  ARGS+=( "--message" "${MESSAGE}" )
+fi
+if [ -n "$INPUT" ]; then
+  ARGS+=( "--input" "${INPUT}" )
+fi
+if [ -n "$TRUSTED_KEYSTORE" ]; then
+  ARGS+=( "--trusted-keystore" "${TRESTED_KEYSTORE}" )
+fi
+
+# Transforming the space separated parameter into multiple single-value parameters. This is necessary, since it is not
+# possible in a Github Action to specify the same parameter multiple times as we do for teamscale-upload.
+for linenumber in $APPEND_TO_MESSAGE; do
+  ARGS+=( "--append-to-message" "$linenumber" )
+done
+if [[ "$STACKTRACE" == "true" ]]; then
+  ARGS+=( "--stacktrace" )
+fi
+if [[ "$INSECURE" == "true" ]]; then
+  ARGS+=( "--insecure" )
+fi
+
+# Transforming the space separated filenames into an array of filenames
+read -r -a FILES_ARRAY <<< "$FILES"
+ARGS+=( "${FILES_ARRAY[@]}" )
+
+./teamscale-upload "${ARGS[@]}"
