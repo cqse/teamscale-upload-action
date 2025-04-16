c9cc08a3b50a1a8b31227922b3d621957b8aca4a
Alexander von Rhein <rhein@cqse.eu>
2025-04-16 09:02:12 +0200

    TS-42220 fix TRESTED_KEYSTORE typo, support --timeout, make --format optional, reorder parameters

--- src/run-teamscale-upload.sh
+++ src/run-teamscale-upload.sh
@@ -10,9 +10,12 @@ fi
 chmod +x teamscale-upload;
 
 # mandatory arguments that have to be always present
-ARGS=( "--server" "$SERVER" "--project" "$PROJECT" "--user" "$USER" "--partition" "$PARTITION" "--accesskey" "$ACCESSKEY" "--format" "$FORMAT" )
+ARGS=( "--server" "$SERVER" "--project" "$PROJECT" "--user" "$USER" "--accesskey" "$ACCESSKEY" "--partition" "$PARTITION" )
 
 # optional parameters. We only use them if they have been set
+if [ -n "$FORMAT" ]; then
+  ARGS+=( "--format" "$FORMAT" )
+fi
 if [ -n "$REVISION" ]; then
   ARGS+=( "--commit" "$REVISION" )
 fi
@@ -22,14 +25,20 @@ fi
 if [ -n "$BRANCH_AND_TIMESTAMP" ]; then
   ARGS+=( "--branch-and-timestamp" "$BRANCH_AND_TIMESTAMP" )
 fi
+if [[ "$MOVE_TO_LAST_COMMIT" == "true" ]]; then
+  ARGS+=( "--move-to-last-commit" )
+fi
 if [ -n "$MESSAGE" ]; then
   ARGS+=( "--message" "${MESSAGE}" )
 fi
 if [ -n "$INPUT" ]; then
   ARGS+=( "--input" "${INPUT}" )
 fi
+if [[ "$INSECURE" == "true" ]]; then
+  ARGS+=( "--insecure" )
+fi
 if [ -n "$TRUSTED_KEYSTORE" ]; then
-  ARGS+=( "--trusted-keystore" "${TRESTED_KEYSTORE}" )
+  ARGS+=( "--trusted-keystore" "${TRUSTED_KEYSTORE}" )
 fi
 
 # Transforming the space separated parameter into multiple single-value parameters. This is necessary, since it is not
@@ -37,18 +46,16 @@ fi
 for linenumber in $APPEND_TO_MESSAGE; do
   ARGS+=( "--append-to-message" "$linenumber" )
 done
-if [[ "$MOVE_TO_LAST_COMMIT" == "true" ]]; then
-  ARGS+=( "--move-to-last-commit" )
-fi
-if [[ "$STACKTRACE" == "true" ]]; then
-  ARGS+=( "--stacktrace" )
-fi
-if [[ "$INSECURE" == "true" ]]; then
-  ARGS+=( "--insecure" )
-fi
 
 # Transforming the space separated filenames into an array of filenames
 read -r -a FILES_ARRAY <<< "$FILES"
 ARGS+=( "${FILES_ARRAY[@]}" )
+
+if [[ "$STACKTRACE" == "true" ]]; then
+  ARGS+=( "--stacktrace" )
+fi
+if [ -n "$TIMEOUT" ]; then
+  ARGS+=( "--timeout" "${TIMEOUT}" )
+fi
 
 ./teamscale-upload "${ARGS[@]}"
